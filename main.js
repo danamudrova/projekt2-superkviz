f1e6647f5e9b219ad6426c8b7b64ce3538a28f89
Dana <danamudrova@gmail.com>
2022-04-09 12:01:15 +0200

    [feat]finish of the click result (counting, result page)

--- main.js
+++ main.js
@@ -22,9 +22,6 @@ let poleOtazek = [{
 }]
 
 
-//první otázka a pořadí
-    changeQuestion(poleOtazek[0],'1/3')
-    
 
 //STRANKA    
 //div pro obsah
@@ -39,11 +36,9 @@ picture.className='foto';
 //fotka
 let photo = document.createElement('img');
 photo.id='obrazek';
-photo.src = poleOtazek[0].foto;
-
 
 //zabalení do divu fotky
-picture.appendChild(photo)
+picture.appendChild(photo);
 
 //zabalení fotky do divu obsahu
 content.appendChild(picture);
@@ -59,87 +54,104 @@ answers.id='odpovedi';
 
 //odpovědi
 let answer1 = document.createElement('li');
-answer1.className ='odpoved'
+answer1.className ='odpoved';
 answer1.setAttribute('data-odpoved', "0");
 
-
 let answer2 = document.createElement('li');
-answer2.className ='odpoved'
+answer2.className ='odpoved';
 answer2.setAttribute('data-odpoved', "1");
 
-
 let answer3 = document.createElement('li');
-answer3.className ='odpoved'
+answer3.className ='odpoved';
 answer3.setAttribute('data-odpoved', "2");
-
 
 //zabalení otázek do divu otazek
 
-answers.appendChild(answer1)
-answers.appendChild(answer2)
-answers.appendChild(answer3)
+answers.appendChild(answer1);
+answers.appendChild(answer2);
+answers.appendChild(answer3);
 
 //zabalení do divu možností
-choice.appendChild(answers)
+choice.appendChild(answers);
 
 //zabaleni možností do divu obsahu
-content.appendChild(choice)
-
+content.appendChild(choice);
 
 //zabalení všeho do divu kviz
 let quiz = document.querySelector('.kviz');
-quiz.appendChild(content)
+quiz.appendChild(content);
 
-changeAnswersPhoto(poleOtazek[0])
-//funkce pro nahrazení hodnot z dalšího pole
-function newQuestion (){
+//iniciální otázky
+changeQuestion(poleOtazek[0],'1/3')
+
+//funkce měniče obsahu stránky
+function changeQuestion(q,order){
+document.querySelector('#otazka').textContent = q.otazka;
+document.querySelector('#poradi').textContent = order;
+answer1.innerHTML = q.odpoved[0];
+answer2.innerHTML = q.odpoved[1];
+answer3.innerHTML = q.odpoved[2];
+photo.src = q.foto;
 }
-
+ 
 //všechny otázky jsou klikatelné
 const odpoved = document.querySelectorAll('.odpoved')
 odpoved.forEach((odpoved) => {
-    odpoved.addEventListener('click', result)
+    odpoved.addEventListener('click', result);
 })
 
 
-
-// function yourAnswer(udalost){
-//     
-//     
-//     //nová otázka a pořadí
-//     changeQuestion(poleOtazek[1],'2/3')
-//     //a nové otázky ¨
-//    changeAnswersPhoto(poleOtazek[1])
-//    result()
-    
-//}
-
-
- function changeQuestion(q,order){
- document.querySelector('#otazka').textContent = q.otazka;
- document.querySelector('#poradi').textContent = order}
-
-function changeAnswersPhoto(q){
-    answer1.innerHTML = q.odpoved[0];
-    answer2.innerHTML = q.odpoved[1];
-    answer3.innerHTML = q.odpoved[2];
-    photo.src = q.foto;
-}
-
-//  //pro výsledek se musí porovnat čísla vybraných odpovědí (data set) s indexem správných odpovědí (přes if) a podle toho se zobrazí výsledek počtu odpovědí (počítadlo?)
+//co se po kliku stane (nová otázka a pořadí, kontrola, skore, vložení odpovědí do výsledku a nakonec zobrazení výsledku)
 function result(udalost){
-    document.querySelector('.kviz').style.display = 'none'
-    document.querySelector('.vysledek').style.display = 'block'
+    let myChoice = udalost.target.dataset.odpoved
 
-    //kontrola odpovědí
-    if (poleOtazek[0].spravnaOdpoved===udalost.target.dataset.odpoved){score()}
-    if (poleOtazek[1].spravnaOdpoved===udalost.target.dataset.odpoved){score()}
-    if (poleOtazek[2].spravnaOdpoved===udalost.target.dataset.odpoved){score()}
-    if (counter!==3){console.log('propadák')}
+    if (document.querySelector('#poradi').innerHTML==='1/3'){
+        changeQuestion(poleOtazek[1],'2/3');
+        if (poleOtazek[0].spravnaOdpoved===myChoice){
+            score();
+            document.querySelector('#tvoje1').innerHTML = 'Tvoje odpověď je SPRÁVNÁ';
+        }
+        else {
+            document.querySelector('#tvoje1').innerHTML+='Tvoje odpověď: '+ '<span>' + poleOtazek[0].odpoved[myChoice] + '</span>';
+        } 
+    }
+    else if (document.querySelector('#poradi').innerHTML==='2/3'){
+        changeQuestion(poleOtazek[2],'3/3');
+        if (poleOtazek[1].spravnaOdpoved===myChoice){
+            score();
+            document.querySelector('#tvoje2').innerHTML = 'Tvoje odpověď je SPRÁVNÁ';
+        }
+        else {
+            document.querySelector('#tvoje2').innerHTML+='Tvoje odpověď: '+ '<span>' + poleOtazek[1].odpoved[myChoice] + '</span>';
+        } 
+    }
+    else if (document.querySelector('#poradi').innerHTML==='3/3'){ 
+        if (poleOtazek[2].spravnaOdpoved===myChoice){
+            score();
+            document.querySelector('#tvoje3').innerHTML = 'Tvoje odpověď je SPRÁVNÁ';
+        }
+        else {
+            document.querySelector('#tvoje3').innerHTML+='Tvoje odpověď: '+ '<span>' + poleOtazek[2].odpoved[myChoice] + '</span>';
+        } 
+        document.querySelector('.kviz').style.display = 'none';
+        document.querySelector('.vysledek').style.display = 'block';
+        let myResult = document.getElementById('soucet-odpovedi').innerText
+
+        if (myResult==='1') {
+            document.getElementById('procenta').innerText = '33';
+        }
+        else if (myResult==='2') {
+            document.getElementById('procenta').innerText = '66';
+        }
+        else if(myResult==='3') {
+            document.getElementById('procenta').innerText = '100';
+            document.getElementById('vitez').textContent='Jsi chytrý jako švec Čič!';
+        }
+    }
 }
 
 //počítadlo
-let counter = 0
+let counter = 1
 function score(){
 	document.getElementById('soucet-odpovedi').innerText = counter++;
 }
